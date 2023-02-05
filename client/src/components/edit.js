f9a4aa9f470e6ceebb2c5cafb76eaa9d3b33fef1
unknown <dhtua124@example.com>
2023-02-05 20:19:28 +0200

    deleted, unneccesary files, added designs and fixed bugs

---
+++ client/src/components/edit.js
@@ -0,0 +1,142 @@
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router";
+ 
+export default function Edit() {
+ const [form, setForm] = useState({
+   name: "",
+   position: "",
+   level: "",
+   records: [],
+ });
+ const params = useParams();
+ const navigate = useNavigate();
+ 
+ useEffect(() => {
+   async function fetchData() {
+     const id = params.id.toString();
+     const response = await fetch(`http://localhost:5000/record/${params.id.toString()}`);
+ 
+     if (!response.ok) {
+       const message = `An error has occurred: ${response.statusText}`;
+       window.alert(message);
+       return;
+     }
+ 
+     const record = await response.json();
+     if (!record) {
+       window.alert(`Record with id ${id} not found`);
+       navigate("/");
+       return;
+     }
+ 
+     setForm(record);
+   }
+ 
+   fetchData();
+ 
+   return;
+ }, [params.id, navigate]);
+ 
+ // These methods will update the state properties.
+ function updateForm(value) {
+   return setForm((prev) => {
+     return { ...prev, ...value };
+   });
+ }
+ 
+ async function onSubmit(e) {
+   e.preventDefault();
+   const editedPerson = {
+     name: form.name,
+     position: form.position,
+     level: form.level,
+   };
+ 
+   // This will send a post request to update the data in the database.
+   await fetch(`http://localhost:5000/update/${params.id}`, {
+     method: "POST",
+     body: JSON.stringify(editedPerson),
+     headers: {
+       'Content-Type': 'application/json'
+     },
+   });
+ 
+   navigate("/");
+ }
+ 
+ // This following section will display the form that takes input from the user to update the data.
+ return (
+   <div style={{direction:"rtl",textAlign:"right"}}>
+     <h3>עדכון פרטי חייל</h3>
+     <form onSubmit={onSubmit}>
+       <div className="form-group">
+         <label htmlFor="name">שם: </label>
+         <input
+           type="text"
+           className="form-control"
+           id="name"
+           value={form.name}
+           onChange={(e) => updateForm({ name: e.target.value })}
+         />
+       </div>
+       <div className="form-group">
+         <label htmlFor="position">תפקיד: </label>
+         <input
+           type="text"
+           className="form-control"
+           id="position"
+           value={form.position}
+           onChange={(e) => updateForm({ position: e.target.value })}
+         />
+       </div>
+       <div className="form-group">
+         <div className="form-check form-check-inline">
+           <input
+             className="form-check-input"
+             type="radio"
+             name="positionOptions"
+             id="positionטוראי"
+             value="טוראי"
+             checked={form.level === "טוראי"}
+             onChange={(e) => updateForm({ level: e.target.value })}
+           />
+           <label htmlFor="positionטוראי" className="form-check-label">טוראי</label>
+         </div>
+         <div className="form-check form-check-inline">
+           <input
+             className="form-check-input"
+             type="radio"
+             name="positionOptions"
+             id="positionרב'ט"
+             value="רב'ט"
+             checked={form.level === "רב'ט"}
+             onChange={(e) => updateForm({ level: e.target.value })}
+           />
+           <label htmlFor="positionרב'ט" className="form-check-label">רב'ט</label>
+         </div>
+         <div className="form-check form-check-inline">
+           <input
+             className="form-check-input"
+             type="radio"
+             name="positionOptions"
+             id="positionסמל"
+             value="סמל"
+             checked={form.level === "סמל"}
+             onChange={(e) => updateForm({ level: e.target.value })}
+           />
+           <label htmlFor="positionסמל" className="form-check-label">סמל</label>
+       </div>
+       </div>
+       <br />
+ 
+       <div className="form-group">
+         <input
+           type="submit"
+           value="עדכון פרטי חייל"
+           className="btn btn-primary"
+         />
+       </div>
+     </form>
+   </div>
+ );
+}
---
+++ client/src/components/navbar.js
@@ -0,0 +1,43 @@
+import React from "react";
+ 
+// We import bootstrap to make our application look better.
+import "bootstrap/dist/css/bootstrap.css";
+import "./../style.css"
+// We import NavLink to utilize the react router.
+import { NavLink } from "react-router-dom";
+ 
+// Here, we display our Navbar
+export default function Navbar() {
+ return (
+   <div>
+     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+       <button
+         className="navbar-toggler"
+         type="button"
+         data-toggle="collapse"
+         data-target="#navbarSupportedContent"
+         aria-controls="navbarSupportedContent"
+         aria-expanded="false"
+         aria-label="Toggle navigation"
+       >
+         <span className="navbar-toggler-icon"></span>
+       </button>
+ 
+       <div className="collapse navbar-collapse" id="navbarSupportedContent" style={{ direction: "rtl" }}>
+         <ul className="navbar-nav ml-auto">
+           <li className="nav-item">
+             <NavLink className="nav-link1" to="/create">
+                 תיצור חייל
+             </NavLink>
+           </li>
+           <li className="nav-item">
+             <NavLink className="nav-link2" to="https://www.idf.il/%D7%90%D7%AA%D7%A8%D7%99-%D7%99%D7%97%D7%99%D7%93%D7%95%D7%AA/%D7%90%D7%92%D7%A3-%D7%94%D7%AA%D7%A7%D7%A9%D7%95%D7%91-%D7%95%D7%94%D7%94%D7%92%D7%A0%D7%94-%D7%91%D7%A1%D7%91-%D7%A8/%D7%9B%D7%9C-%D7%94%D7%9B%D7%AA%D7%91%D7%95%D7%AA/%D7%9B%D7%AA%D7%91%D7%95%D7%AA-%D7%9B%D7%9C%D7%9C%D7%99%D7%95%D7%AA/%D7%9E%D7%9E%D7%A8-%D7%9D/">
+                 <img className="logo" src = {require('./../Images/Mamram.png')} />
+             </NavLink>
+           </li>
+         </ul>
+       </div>
+     </nav>
+   </div>
+ );
+}
